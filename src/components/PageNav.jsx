f188923e07ed841b52ee08ab44aafdb92ccfab44
girardpao <girardpao@gmail.com>
2023-10-25 04:59:12 +0800

    added pagenav and pagenotfound

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -14,19 +14,12 @@ const PageNav = () => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
+            <Nav className="me-auto my-2 my-lg-0" navbarScroll>
               <Nav.Link as={Link} to="/schedule">
                 Schedule
               </Nav.Link>
               <Nav.Link as={Link} to="/contactUs">
                 Contact Us
-              </Nav.Link>
-              <Nav.Link as={Link} to="/login">
-                Login
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,14 @@
+import styles from "./PageNotFound.module.css";
+
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div
+      className="d-flex align-items-center justify-content-center"
+      id={styles.error}
+    >
+      <h1 className={styles.notFound}>:( Page Not Found</h1>
+    </div>
+  );
 };
 
 export default PageNotFound;
