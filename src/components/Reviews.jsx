e9d96c84a7f9d9591e48e2a9ade99880a9358ac1
girardpao <girardpao@gmail.com>
2023-11-06 17:25:27 +0800

    fixing carousel

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,12 +1,17 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+// import Card from "react-bootstrap/Card";
+import kayh from "../assets/kayh.jpg";
+import kayh2 from "../assets/kayh2.jpg";
+import kayh3 from "../assets/kayh3.jpg";
+import Carousel from "react-bootstrap/Carousel";
+import styles from "./Reviews.module.css";
 
 const Reviews = () => {
   return (
     <div>
-      <Container className="mt-5 mb-5 text">
+      {/* <Container className="mt-5 mb-5 text">
         <h3 className="text-center mb-3">Reviews</h3>
         <Row>
           <Col>
@@ -38,6 +43,98 @@ const Reviews = () => {
               </Card.Body>
             </Card>
           </Col>
+        </Row>
+      </Container> */}
+      <Container className="container text-center mt-5">
+        <Row className="row">
+          <Carousel fade>
+            <Carousel.Item className="d-flex">
+              <Col md={4}>
+                <div className="single-box"></div>
+                <div className="img-area">
+                  <img src={kayh} className={styles.kayh1} />
+                </div>
+                <div className="img-text">
+                  <h2>Person One</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptates quasi corrupti neque consequuntur, excepturi
+                    enim.
+                  </p>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className="single-box"></div>
+                <div className="img-area">
+                  <img src={kayh2} className={styles.kayh2} />
+                </div>
+                <div className="img-text">
+                  <h2>Person Two</h2>
+                  <p>
+                    ipsum dolor sit amet consectetur adipisicing elit. Rem
+                    voluptate culpa eaque commodi animi voluptas corporis.
+                  </p>
+                </div>
+              </Col>
+              <Col cmd={4}>
+                <div className="single-box"></div>
+                <div className="img-area">
+                  <img src={kayh3} className={styles.kayh3} />
+                </div>
+                <div className="img-text">
+                  <h2>Person Three</h2>
+                  <p>
+                    dolor sit amet consectetur adipisicing elit. Quos eveniet
+                    dolor natus sunt, eaque vitae voluptatem. Modi.
+                  </p>
+                </div>
+              </Col>
+            </Carousel.Item>
+            <Carousel.Item className="d-flex">
+              <Col md={4}>
+                <div className="single-box"></div>
+                <div className="img-area">
+                  <img src={kayh3} className={styles.kayh1} />
+                </div>
+                <div className="img-text">
+                  <h2>Person four</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptates quasi corrupti neque consequuntur, excepturi
+                    enim.
+                  </p>
+                </div>
+              </Col>
+              <Col md={4}>
+                <div className="single-box"></div>
+                <div className="img-area">
+                  <img src={kayh} className={styles.kayh2} />
+                </div>
+                <div className="img-text">
+                  <h2>Person five</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptates quasi corrupti neque consequuntur, excepturi
+                    enim.
+                  </p>
+                </div>
+              </Col>
+              <Col cmd={4}>
+                <div className="single-box"></div>
+                <div className="img-area">
+                  <img src={kayh2} className={styles.kayh3} />
+                </div>
+                <div className="img-text">
+                  <h2>Person six</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptates quasi corrupti neque consequuntur, excepturi
+                    enim.
+                  </p>
+                </div>
+              </Col>
+            </Carousel.Item>
+          </Carousel>
         </Row>
       </Container>
     </div>
