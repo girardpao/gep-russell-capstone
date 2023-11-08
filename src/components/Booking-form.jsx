c126aa14dabb54c5d579c0a2fe2c97e41b68231a
girardpao <girardpao@gmail.com>
2023-11-08 17:10:23 +0800

    added date input

--- src/components/Booking-form.jsx
+++ src/components/Booking-form.jsx
@@ -2,19 +2,34 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import styles from "./Booking-form.module.css";
 
 const bookingForm = () => {
   return (
     <div>
       <Container className="mt-5">
-        <Row className="mb-2">
-          <Form.Group className="mb-3">
-            <Form.Label>Departing:</Form.Label>
-            <Form.Select>
-              <option>-- Choose --</option>
-            </Form.Select>
-          </Form.Group>
-        </Row>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="inputGroup-sizing-default">Date</InputGroup.Text>
+          <Form.Control
+            type="date"
+            aria-label="Date"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
+
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="inputGroup-sizing-default">
+            Passengers
+          </InputGroup.Text>
+          <Form.Control
+            type="number"
+            min={1}
+            max={30}
+            aria-label="Date"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
         <Row className="mb-2">
           <Form.Group className="mb-3">
             <Form.Label>From:</Form.Label>
@@ -40,7 +55,7 @@ const bookingForm = () => {
           </Form.Group>
         </Row>
         <div className="text-center">
-          <Button variant="success">Proceed</Button>
+          <Button className={styles.search}>Search</Button>
         </div>
       </Container>
     </div>
